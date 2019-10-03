d8236371af114c588df25b94f207dd6355dac30a
Derek S <dschwan21@me.com>
2019-10-02 23:43:35 -0700

    added room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,17 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room():
+    def __init__(self, name="", description=""):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f"Room: {self.name} is {self.description}"
+    def __repr__(self):
+        return f"Player({repr(self.name)}, {repr(self.description)})"
+
+
+outs = Room("outside", "this is out of the house")
+
+print(repr(outs))
